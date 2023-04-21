6d221c09fc7ad6c84f96e95f81231d3746497f40
Igor <Igor.ananyev1989@gmail.com>
2023-04-21 20:44:51 +0300

    all commit

---
+++ Homework/test_hw.py
@@ -0,0 +1,14 @@
+def test_passing():
+    assert ('home', 'work') == ('home', "work");
+
+
+def testing_passing2():
+    assert "QA" == "QA"
+
+
+def test_passing1():
+    assert not (1,1,3,5) == (2,3,5)
+
+
+def test_passing3():
+    assert 'QC' == "QA"
---
+++ python_training/task_1_numbers.py
@@ -0,0 +1,23 @@
+a = 2
+print(a, 'относится к типу ', type(a))
+
+
+b = 15.0001
+print(b, 'относиится к типу ', type(b))
+
+
+c = 1+2j
+print(c, "Комплексное число? ", isinstance(c, complex))
+
+
+print(6 + 2) # 8
+print(6 - 2) # 4
+print(6 * 2)
+print(6 / 2)
+
+print(7 / 2)
+print(7 // 2) # получение целго числа от деления.
+
+print(7 % 2)
+
+print(6 ** 2)
